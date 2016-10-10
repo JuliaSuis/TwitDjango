efe22070adf6ffbfc58026444ea42d49f42c869f
IuliiaSem <iuliia.sem9@gmail.com>
2016-10-10 11:36:01 +0200

    locations changed

--- sentiment/code/mainfile.py
+++ sentiment/code/mainfile.py
@@ -16,13 +16,16 @@ print(location)
 
 #query_location=location + '/sentiment/code/dataset/' + query +'.json'
 
+dictionary=location + '/sentiment/code/dataset/' +'AFINN-111.txt'
+tweet_file=location + '/sentiment/code/dataset/' +'tweet_out.txt'
+sentiment_location= location + '/sentiment/static/img/' +'sentiment.png'
+wc_location= location + '/sentiment/static/img/' +'wc.png'
 
-
-dictionary='/home/julia/code/DjangoTM/twitTMP/sentiment/code/dataset/' +'AFINN-111.txt'
-tweet_file='/home/julia/code/DjangoTM/twitTMP/sentiment/code/dataset/' +'tweet_out.txt'
-sentiment_location='/home/julia/code/DjangoTM/twitTMP/sentiment/static/img/' +'sentiment.png'
-wc_location='/home/julia/code/DjangoTM/twitTMP/sentiment/static/img/' +'wc.png'
 url_sentiment=location+ '/sentiment/code/dataset/' +'url_sentiment.txt'
 hashtag_file=location+ '/sentiment/code/dataset/' +'hashtag.txt'
 hashtag_graph=location+ '/sentiment/static/img/' +'hashtag.png'
 
+#dictionary='/home/julia/code/DjangoTM/twitTMP/sentiment/code/dataset/' +'AFINN-111.txt'
+#tweet_file='/home/julia/code/DjangoTM/twitTMP/sentiment/code/dataset/' +'tweet_out.txt'
+#sentiment_location='/home/julia/code/DjangoTM/twitTMP/sentiment/static/img/' +'sentiment.png'
+#wc_location='/home/julia/code/DjangoTM/twitTMP/sentiment/static/img/' +'wc.png'
